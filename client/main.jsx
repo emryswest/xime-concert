dd9f5358d21d20b2d89ada6417f3962c0162d8ae
Emrys West <westemrys@gmail.com>
2018-11-20 20:55:44 -0700

    user login and toggle completed

--- client/main.jsx
+++ client/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- import App from '/imports/ui/App'
- import FileUpload from '/imports/ui/FileUpload'
 
-//Meteor.startup(() => {
-//  render(<App />, document.getElementById('react-target'));
-//});
+import '../imports/startup/accounts-config.js';
+import App from '/imports/ui/App'
+import FileUpload from '/imports/ui/FileUpload'
 
 Meteor.startup(() => {
-  render(<FileUpload />, document.getElementById('react-target'));
+  render(<App />, document.getElementById('react-target'));
 });
+
+//Meteor.startup(() => {
+//  render(<FileUpload />, document.getElementById('react-target'));
+//});
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -32,8 +32,9 @@ const taskClassName = this.props.task.checked ? 'checked' : '';
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
-
-        <span className="text">{this.props.task.text}</span>
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
       </li>
     );
   }
